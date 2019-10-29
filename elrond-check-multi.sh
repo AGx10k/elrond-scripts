22077fe2cd89cdcafa5c8be9c1585145e6de2308
ag <48479505+AGx10k@users.noreply.github.com>
2019-10-29 13:19:48 +0300

    Update elrond-check-multi.sh

--- elrond-check-multi.sh
+++ elrond-check-multi.sh
@@ -36,9 +36,9 @@ for i in "${nodes[@]}"; do
                 if [ "$shard" -eq "4294967295" ]; then
                         shard="-1"
                 fi
-                echo "$shard,"$(jq -r '[.details | .erd_count_accepted_blocks  // "-" , .erd_count_leader // "-" , .erd_is_syncing // "-", .erd_node_type // "-", .erd_nonce // "-", .erd_current_round // "-" , .erd_synchronized_round // "-" ]| @csv' <<< "$curlout")",$i"
+                echo "$shard,"$(jq -r '[.details | .erd_count_accepted_blocks  // "-" , .erd_count_leader // "-" , .erd_is_syncing // "-", .erd_node_type // "-", .erd_nonce // "-", .erd_current_round // "-" , .erd_synchronized_round // "-", .erd_consensus_state // "-", .erd_num_connected_peers // "-" ]| @csv' <<< "$curlout")",$i"
         fi
 done
 ) | sort -n -t , ${sortKEYS} \
-  | sed '1 i\erd_shard_id,erd_count_accepted_blocks,erd_count_leader,erd_is_syncing,erd_node_type,erd_nonce,erd_current_round,erd_synchronized_round,NAME,addr' \
+  | sed '1 i\shard,blocks,count_leader,syncing,node_type,nonce,cur_round,synced_round,consensus_state,peers,NAME,addr' \
   | column -t -s,
